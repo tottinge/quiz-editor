38172f9e38912ea6672cd95083ee54c4c26c8c05
Tim Ottinger <tottinge@industriallogic.com>
2025-04-09 17:56:14 +0100

    feat: Start work on the resouce sub-component

---
+++ src/Resource.js
@@ -0,0 +1,29 @@
+export function Resource(props) {
+    return <div className="w3-container w3-card w3-grid">
+        <div classname="w3-row" style={{bgcolor: "red"}}>
+
+            <div className="w3-col">
+                <label>Resource</label>
+                <input
+                    className="w3-input"
+                    id="description"
+                    name="description"
+                    placeholder="Enter description here:"
+                    type='text'
+                    value={props.description}
+                />
+                <input
+                    className="w3-input"
+                    placeholder="Enter URL here:"
+                    id="url"
+                    name="url"
+                    type="url"
+                    value={props.url}
+                />
+            </div>
+            <div className="w3-card">
+                <iframe src={props.url} title={props.description}/>
+            </div>
+        </div>
+    </div>
+}
--- src/Resource.test.js
+++ src/Resource.test.js
@@ -1,54 +1,23 @@
-// noinspection JSCheckFunctionSignatures
-
-import {ResourceList} from "./ResourceList";
+import {Resource} from "./Resource";
 import {render, screen} from "@testing-library/react";
-import {expect, it} from '@jest/globals';
-import userEvent from "@testing-library/user-event";
+import {expect, it} from "@jest/globals";
+
+it("can render an empty resource", () => {
+    render(<Resource />);
+
+    expect(screen.getByLabelText(/description/i)).toBeVisible();
+    expect(screen.getByLabelText(/url/i)).toBeVisible();
+});
 
 
-it("renders an empty list of resources", ()=> {
-    render(<ResourceList/>)
+it("can render a populated resource", () => {
+    render(<Resource
+        description="Jabberwocky, The Poem by Robert Lewis Stevenson"
+        url="https://www.poetryfoundation.org/poems/42916/jabberwocky"
+    />);
+    expect(screen.getByDisplayValue(/the poem by/i)).toBeVisible()
+    expect(screen.getByDisplayValue(/poetryfoundation/i)).toBeVisible()
+});
 
-    const title = screen.getByText(/resource list/i)
-    expect(title).toBeInTheDocument()
-    expect(title).toBeVisible()
-
-    const newItemField = screen.getByPlaceholderText(/resource link/i)
-    expect(newItemField).toBeVisible()
-})
-
-it("renders a non-empty list of resources", ()=> {
-    render(<ResourceList resources={["affirmative", "negative"]}/>)
-    const resources = screen.getAllByRole('listitem')
-    expect(resources).toHaveLength(3)
-})
-
-it("can add a resource when users hit Enter", ()=> {
-    const resourceText = "pressed enter to add"
-    render(<ResourceList resources={[]}/>)
-
-    const newItemField = screen.getByPlaceholderText(/resource link/i)
-    userEvent.type(newItemField, resourceText)
-    userEvent.keyboard("{enter}")
-
-    // expect one resource and one input
-    const resources = screen.getAllByRole('listitem')
-    expect(resources).toHaveLength(2)
-    expect(resources[0]).toHaveTextContent(resourceText)
-})
-
-it("can add a resource when users tab off the input field", ()=> {
-    const resourceText = "Enter By Tabbing"
-    render(<ResourceList resources={[]}/>)
-
-    const newItemField = screen.getByPlaceholderText(/resource link/i)
-    userEvent.type(newItemField, resourceText)
-    userEvent.tab()
-
-    // expect one resource and one input
-    const resources = screen.getAllByRole('listitem')
-    expect(resources).toHaveLength(2)
-    expect(resources[0]).toHaveTextContent(resourceText)
-})
-
-it("can remove a resource", ()=> {})
+it("can edit the description", () => {});
+it("can enter a valid URL", ()=>{})
