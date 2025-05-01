e55acab11cc0783f19ea2a4febd4af6474d6e201
Tim Ottinger <tottinge@industriallogic.com>
2025-05-01 09:43:54 -0500

    test: added placeholder test outlines

--- src/Quiz.test.js
+++ src/Quiz.test.js
@@ -1,4 +1,4 @@
-import {expect} from "@jest/globals";
+import {expect, it} from "@jest/globals";
 import {render, screen} from "@testing-library/react";
 import {Quiz} from "./Quiz";
 
@@ -6,3 +6,17 @@ it("renders a quiz with no questions", ()=>{
     render(<Quiz />)
     expect(screen.getByRole("heading")).toBeInTheDocument()
 })
+
+it("renders a quiz with questions", ()=>{
+    render(<Quiz />)
+})
+
+it("can add a question", ()=>{})
+
+it("can remove a question", ()=>{})
+
+it("can open to edit a question", ()=>{})
+
+it("can close to hide question details", ()=>{})
+
+it("can move a question in the list", ()=>{})
